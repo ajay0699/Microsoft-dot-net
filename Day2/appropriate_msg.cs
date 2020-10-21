9fc6bd50b0111450c36c031dbcec99d9c3d9f66b
ajay0699 <61387962+ajay0699@users.noreply.github.com>
2020-10-22 01:44:57 +0530

    Add files via upload

---
+++ Day2/appropriate_msg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+   
+namespace Day2_Appropriate_msg
+{
+ class Program
+    {
+        public static int sqr(int x)
+        {
+            return x * x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a no");
+            string x = Console.ReadLine();
+            int f;
+            bool a1 = int.TryParse(x,out f);
+            Console.WriteLine(a1);
+            if (a1 == true)
+            {
+                int result = sqr(f);
+                Console.WriteLine("the square of no is :" + result);
+                Console.ReadLine();
+            }
+            else {
+                Console.WriteLine("please enter proper input");
+                Console.ReadLine();
+            }
+        }
+    }
+}
